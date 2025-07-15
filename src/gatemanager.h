5d696540e5882b345993f7c007b18928b0491c16
ƬЩIZƬΣDЩIZΛЯD <joshwall488@gmail.com>
2025-07-15 02:53:08 -0400

    Add Gate and GateManager headers

---
+++ src/gatemanager.h
@@ -0,0 +1,20 @@
+#ifndef FS_GATEMANAGER_H
+#define FS_GATEMANAGER_H
+
+#include "gate.h"
+#include <map>
+#include <cstdint>
+
+class GateManager {
+	public:
+		Gate* spawnGate(const Position& pos, GateRank rank, GateType type);
+		Gate* getGate(uint32_t gateId);
+		void update();
+		void removeGate(uint32_t gateId);
+
+	private:
+		uint32_t generateGateId();
+		std::map<uint32_t, Gate> gates;
+};
+
+#endif // FS_GATEMANAGER_H
