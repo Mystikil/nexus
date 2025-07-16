c0cb5fa8e6241f8dbaba62202cbbd7396da8e1c9
ƬЩIZƬΣDЩIZΛЯD <joshwall488@gmail.com>
2025-07-15 22:08:25 -0400

    Avoid overwriting globals when registering gate enums

--- src/luascript.cpp
+++ src/luascript.cpp
@@ -1918,25 +1918,16 @@ void LuaScriptInterface::registerFunctions() {
         registerTable(L, "GateRank");
         registerTable(L, "GateType");
 
-        registerEnum(L, GateRank::E);
-        registerEnumIn(L, "GateRank", GateRank::E);
-        registerEnum(L, GateRank::D);
-        registerEnumIn(L, "GateRank", GateRank::D);
-        registerEnum(L, GateRank::C);
-        registerEnumIn(L, "GateRank", GateRank::C);
-        registerEnum(L, GateRank::B);
-        registerEnumIn(L, "GateRank", GateRank::B);
-        registerEnum(L, GateRank::A);
-        registerEnumIn(L, "GateRank", GateRank::A);
-        registerEnum(L, GateRank::S);
-        registerEnumIn(L, "GateRank", GateRank::S);
+       registerEnumIn(L, "GateRank", GateRank::E);
+       registerEnumIn(L, "GateRank", GateRank::D);
+       registerEnumIn(L, "GateRank", GateRank::C);
+       registerEnumIn(L, "GateRank", GateRank::B);
+       registerEnumIn(L, "GateRank", GateRank::A);
+       registerEnumIn(L, "GateRank", GateRank::S);
 
-        registerEnum(L, GateType::NORMAL);
-        registerEnumIn(L, "GateType", GateType::NORMAL);
-        registerEnum(L, GateType::RED);
-        registerEnumIn(L, "GateType", GateType::RED);
-        registerEnum(L, GateType::DOUBLE);
-        registerEnumIn(L, "GateType", GateType::DOUBLE);
+       registerEnumIn(L, "GateType", GateType::NORMAL);
+       registerEnumIn(L, "GateType", GateType::RED);
+       registerEnumIn(L, "GateType", GateType::DOUBLE);
 
 
 	// Use with container:addItem, container:addItemEx and possibly other functions.
