3aac962831c2efc1d6fb44e08414f579be2d5090
ƬЩIZƬΣDЩIZΛЯD <joshwall488@gmail.com>
2025-07-15 20:19:40 -0400

    Fix gate enum table registration order

--- src/luascript.cpp
+++ src/luascript.cpp
@@ -1909,26 +1909,27 @@ void LuaScriptInterface::registerFunctions() {
 
         // Gate system enums
         registerTable(L, "GateRank");
-	registerEnum(L, GateRank::E);
-	registerEnumIn(L, "GateRank", GateRank::E);
-	registerEnum(L, GateRank::D);
-	registerEnumIn(L, "GateRank", GateRank::D);
-	registerEnum(L, GateRank::C);
-	registerEnumIn(L, "GateRank", GateRank::C);
-	registerEnum(L, GateRank::B);
-	registerEnumIn(L, "GateRank", GateRank::B);
-	registerEnum(L, GateRank::A);
-	registerEnumIn(L, "GateRank", GateRank::A);
-	registerEnum(L, GateRank::S);
-	registerEnumIn(L, "GateRank", GateRank::S);
+        registerTable(L, "GateType");
 
-	registerTable(L, "GateType");
-	registerEnum(L, GateType::NORMAL);
-	registerEnumIn(L, "GateType", GateType::NORMAL);
-	registerEnum(L, GateType::RED);
-	registerEnumIn(L, "GateType", GateType::RED);
-	registerEnum(L, GateType::DOUBLE);
-	registerEnumIn(L, "GateType", GateType::DOUBLE);
+        registerEnum(L, GateRank::E);
+        registerEnumIn(L, "GateRank", GateRank::E);
+        registerEnum(L, GateRank::D);
+        registerEnumIn(L, "GateRank", GateRank::D);
+        registerEnum(L, GateRank::C);
+        registerEnumIn(L, "GateRank", GateRank::C);
+        registerEnum(L, GateRank::B);
+        registerEnumIn(L, "GateRank", GateRank::B);
+        registerEnum(L, GateRank::A);
+        registerEnumIn(L, "GateRank", GateRank::A);
+        registerEnum(L, GateRank::S);
+        registerEnumIn(L, "GateRank", GateRank::S);
+
+        registerEnum(L, GateType::NORMAL);
+        registerEnumIn(L, "GateType", GateType::NORMAL);
+        registerEnum(L, GateType::RED);
+        registerEnumIn(L, "GateType", GateType::RED);
+        registerEnum(L, GateType::DOUBLE);
+        registerEnumIn(L, "GateType", GateType::DOUBLE);
 
 
 	// Use with container:addItem, container:addItemEx and possibly other functions.
